1f09dfcc700ff3b03e7ecf1bd6b4d770ee3c6f82
devinlife <devinlifeidea@gmail.com>
2024-05-05 19:33:45 +0900

    Use NOP not TRANSPARENT

--- keyboards/massdrop/alt/keymaps/hack/keymap.c
+++ keyboards/massdrop/alt/keymaps/hack/keymap.c
@@ -16,48 +16,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Windows Layout
     [0] = LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, _______,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, XXXXXXX,
         MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  DF(3),
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   MO(2),
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_F13 , MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,        KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,   KC_F9,          KC_F10,  KC_F11,  KC_F12,  KC_CAPS, KC_SCRL,
-        _______, KC_GRV,  LSFT(KC_GRV), _______, _______, _______, LCTL(KC_BSPC), KC_BSPC, KC_HOME, LCTL(KC_LEFT),  KC_HOME, _______, KC_PAUS, KC_W,    KC_INSERT,
-        _______, KC_WBAK, KC_WFWD,      KC_PGUP, KC_PGDN, _______, KC_LEFT,       KC_DOWN, KC_UP,   KC_RIGHT,       _______, _______,          _______, KC_NUM_LOCK,
-        _______, _______, _______,      _______, _______, _______, KC_APP,        KC_DEL,  KC_END,  LCTL(KC_RIGHT), KC_END,  _______,          KC_VOLU, MO(2),
-        _______, _______, _______,                                                  _______,                        _______, _______, _______, KC_VOLD, _______
+        XXXXXXX, KC_GRV,  LSFT(KC_GRV), XXXXXXX, XXXXXXX, XXXXXXX, LCTL(KC_BSPC), KC_BSPC, KC_HOME, LCTL(KC_LEFT),  KC_HOME, XXXXXXX, KC_PAUS, KC_W,    KC_INSERT,
+        XXXXXXX, KC_WBAK, KC_WFWD,      KC_PGUP, KC_PGDN, XXXXXXX, KC_LEFT,       KC_DOWN, KC_UP,   KC_RIGHT,       XXXXXXX, XXXXXXX,          XXXXXXX, KC_NUM_LOCK,
+        XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, KC_APP,        KC_DEL,  KC_END,  LCTL(KC_RIGHT), KC_END,  XXXXXXX,          KC_VOLU, MO(2),
+        XXXXXXX, XXXXXXX, XXXXXXX,                                                  XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, XXXXXXX
     ),
     [2] = LAYOUT_65_ansi_blocker(
-        _______, LCTL(KC_F13), LCTL(KC_F14), LCTL(KC_F15), _______,     _______, KC_PSCR, RSG(KC_S), _______, _______, _______, _______, _______, _______, _______,
-        _______, _______,      _______,      _______,      LSFT(KC_F4), _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-        KC_F14,  LSA(KC_E),    LSA(KC_F),    RCS(KC_G),    RSG(KC_F12), _______, _______, _______,   _______, _______, _______, _______,          _______, _______,
-        _______, _______,      RCS(KC_B),    _______,      KC_F6,       _______, _______, _______,   _______, _______, _______, _______,          L_BRI  , _______,
-        _______, _______,      _______,                                          _______,                              _______, _______, _______, L_BRD  , _______
+        XXXXXXX, LCTL(KC_F13), LCTL(KC_F14), LCTL(KC_F15), XXXXXXX,     XXXXXXX, KC_PSCR, RSG(KC_S), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX,      XXXXXXX,      XXXXXXX,      LSFT(KC_F4), XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_F14,  LSA(KC_E),    LSA(KC_F),    RCS(KC_G),    RSG(KC_F12), XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX,      RCS(KC_B),    XXXXXXX,      KC_F6,       XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          L_BRI  , XXXXXXX,
+        XXXXXXX, XXXXXXX,      XXXXXXX,                                          XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX, L_BRD  , XXXXXXX
     ),
     // Mac Layout
     [3] = LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, DF(0),
-        MO(4),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  _______,
+        MO(4),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  XXXXXXX,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   MO(5),
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_F13 , MO(5),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [4] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,         KC_F2,          KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,          KC_F9,          KC_F10,  KC_F11,  KC_F12,  LSFT(KC_CAPS), KC_SCRL,
-        _______, KC_GRV,        LSFT(KC_GRV),   _______, _______, _______, LGUI(KC_BSPC), KC_BSPC, LALT(KC_LEFT),  LGUI(KC_LEFT),  KC_HOME, _______, KC_PAUS, KC_M,          KC_INSERT,
-        _______, LALT(KC_LBRC), LALT(KC_RBRC),  KC_PGUP, KC_PGDN, _______, KC_LEFT,       KC_DOWN, KC_UP,          KC_RIGHT,       _______, _______,          _______,       KC_NUM_LOCK,
-        _______, _______,       _______,        _______, _______, _______, _______,       KC_DEL,  LALT(KC_RIGHT), LGUI(KC_RIGHT), KC_END,  _______,          KC_VOLU,       MO(5),
-        _______, _______,       _______,                                                  _______,                                 _______, _______, _______, KC_VOLD,       _______
+        XXXXXXX, KC_GRV,        LSFT(KC_GRV),   XXXXXXX, XXXXXXX, XXXXXXX, LGUI(KC_BSPC), KC_BSPC, LALT(KC_LEFT),  LGUI(KC_LEFT),  KC_HOME, XXXXXXX, KC_PAUS, KC_M,          KC_INSERT,
+        XXXXXXX, LALT(KC_LBRC), LALT(KC_RBRC),  KC_PGUP, KC_PGDN, XXXXXXX, KC_LEFT,       KC_DOWN, KC_UP,          KC_RIGHT,       XXXXXXX, XXXXXXX,          XXXXXXX,       KC_NUM_LOCK,
+        XXXXXXX, XXXXXXX,       XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_DEL,  LALT(KC_RIGHT), LGUI(KC_RIGHT), KC_END,  XXXXXXX,          KC_VOLU,       MO(5),
+        XXXXXXX, XXXXXXX,       XXXXXXX,                                                  XXXXXXX,                                 XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,       XXXXXXX
     ),
     [5] = LAYOUT_65_ansi_blocker(
-        _______, LCTL(KC_F13), LCTL(KC_F14), LCTL(KC_F15), _______,     _______, LSA(KC_3), LSA(KC_4), _______, _______, _______, _______, _______, _______, _______,
-        _______, _______,      _______,      _______,      LSFT(KC_F4), _______, _______,   _______,   _______, _______, _______, _______, _______, _______, _______,
-        KC_F14,  LSA(KC_E),    LSA(KC_F),    RCS(KC_G),    RSG(KC_F12), _______, _______,   _______,   _______, _______, _______, _______,          _______, _______,
-        _______, _______,      RCS(KC_B),    _______,      KC_F4,       _______, _______,   _______,   _______, _______, _______, _______,          L_BRI,   _______,
-        _______, _______,      _______,                                 _______,                                         _______, _______, _______, L_BRD,   _______
+        XXXXXXX, LCTL(KC_F13), LCTL(KC_F14), LCTL(KC_F15), XXXXXXX,     XXXXXXX, LSA(KC_3), LSA(KC_4), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX,      XXXXXXX,      XXXXXXX,      LSFT(KC_F4), XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_F14,  LSA(KC_E),    LSA(KC_F),    RCS(KC_G),    RSG(KC_F12), XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX,      RCS(KC_B),    XXXXXXX,      KC_F4,       XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          L_BRI,   XXXXXXX,
+        XXXXXXX, XXXXXXX,      XXXXXXX,                                 XXXXXXX,                                         XXXXXXX, XXXXXXX, XXXXXXX, L_BRD,   XXXXXXX
     ),
     /*
+    [X] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
     [X] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
